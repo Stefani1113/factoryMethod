3954d811002bc42b27d84b92ecf808ff4da70402
Stefani1113 <stefaniapuerta604@gmail.com>
2026-06-21 17:09:39 -0500

    fix: Correción de service y controller

--- src/main/java/com/ejercicio/factoryMethod/controller/NotificacionController.java
+++ src/main/java/com/ejercicio/factoryMethod/controller/NotificacionController.java
@@ -4,12 +4,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ejercicio.factoryMethod.service.NotificacionService;
+
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
 @RequestMapping("/notificaciones")
+@RequiredArgsConstructor
 
 public class NotificacionController {
     
--- src/main/java/com/ejercicio/factoryMethod/service/NotificacionService.java
+++ src/main/java/com/ejercicio/factoryMethod/service/NotificacionService.java
@@ -1,7 +1,11 @@
 package com.ejercicio.factoryMethod.service;
 
+import org.springframework.stereotype.Service;
+
 import com.ejercicio.factoryMethod.factory.NotificacionFactory;
 import com.ejercicio.factoryMethod.interfaces.Notificacion;
+
+@Service
 
 public class NotificacionService {
     
