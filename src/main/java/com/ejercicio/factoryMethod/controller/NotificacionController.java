1d3cb0deb152f3ff92527efa445e6384061b40d9
Stefani1113 <stefaniapuerta604@gmail.com>
2026-06-21 17:13:23 -0500

    fix: Correción de controller

--- src/main/java/com/ejercicio/factoryMethod/controller/NotificacionController.java
+++ src/main/java/com/ejercicio/factoryMethod/controller/NotificacionController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 public class NotificacionController {
     
-    private NotificacionService service;
+    private final NotificacionService service;
 
     @GetMapping("{tipo}")
     public String enviar(@PathVariable String tipo) {
